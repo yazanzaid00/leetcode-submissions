b212e0a96de5048d0d2c09dce6fd760931cc730c
yazanzaid00 <yazan00zaid@gmail.com>
2026-05-12 11:31:29 +0300

    Add: clone-graph - submission-6

---
+++ Data Structures & Algorithms/clone-graph/submission-6.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+        clone_dict = {node.val:Node(node.val)}
+        # bfs
+        queue = deque([node])
+        while queue:
+            cur_node = queue.popleft()
+            for nei in cur_node.neighbors:
+                if nei.val not in clone_dict:
+                    # the key can be the Node it self? or it can be the vale if i know the value is unique
+                    clone_dict[nei.val] = Node(nei.val)
+                    queue.append(nei)
+                clone_dict[cur_node.val].neighbors.append(clone_dict[nei.val])
+        return clone_dict[node.val]
