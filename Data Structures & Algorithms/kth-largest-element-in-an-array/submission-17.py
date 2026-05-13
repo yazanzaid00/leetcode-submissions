9367df4005812816d7eed3dc72066a10c9b4282c
yazanzaid00 <yazan00zaid@gmail.com>
2026-05-13 15:36:09 +0300

    Add: kth-largest-element-in-an-array - submission-17

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-17.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        min_heap = [num for num in nums[:k]]
+        heapq.heapify(min_heap)
+        for num in nums[k:]:
+            if num > min_heap[0]:
+                heapq.heappushpop(min_heap, num)
+                
+        return min_heap[0]
