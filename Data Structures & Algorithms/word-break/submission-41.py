4c05aef87ce91573acca3feb1d725b35523e7be2
yazanzaid00 <yazan00zaid@gmail.com>
2026-05-13 16:16:22 +0300

    Add: word-break - submission-41

---
+++ Data Structures & Algorithms/word-break/submission-41.py
@@ -0,0 +1,13 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # dp[i] = s[0...i-1] is breakable
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+        for i in range(len(s)):
+            if not dp[i]:
+                continue
+            for word in wordDict:
+                if i + len(word) <= len(s) and s.startswith(word, i):
+                    dp[i + len(word)] = True
+
+        return dp[-1]
